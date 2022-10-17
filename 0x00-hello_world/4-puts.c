5cf0f3a0de2f9f992418823f86cc19c37a78404b
learnatalx <gohatsibahw@gmail.com>
2022-10-17 00:12:30 -0700

    add file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
  * main entry point
+ * return 0 on success
  **/
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
