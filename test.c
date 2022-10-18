28169f597fae61359e8c8f60c7c67bf050900c46
learnatalx <gohatsibahw@gmail.com>
2022-10-18 11:34:02 -0700

    add file

---
+++ test.c
@@ -0,0 +1,9 @@
+/*This is just a test function*/
+#include <stdio.h>
+void main(void)
+{
+/*This is the main function */
+printf("The char you have entered is:");
+puts("string printed");
+putchar('a');
+}
