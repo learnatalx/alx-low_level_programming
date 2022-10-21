f1f44831ee160d6b6290612dbd317240e910fe62
learnatalx <gohatsibahw@gmail.com>
2022-10-21 12:47:56 -0700

    add file

--- 0x04-more_functions_nested_loops/1-main.c
+++ 0x04-more_functions_nested_loops/1-main.c
@@ -1,16 +1,16 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - entry point
+ * main - entry porint
  *
  * Return: 0.
  */
 int main(void)  
 {
-char c;
+char c;  
 c = '0';  
 printf("%c: %d\n", c, _isdigit(c));  
 c = 'a';  
 printf("%c: %d\n", c, _isdigit(c));
-return (0);
+return (0);  
 }
