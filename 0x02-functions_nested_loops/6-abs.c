93c57629f1e69a891c5450fa61053bddc087c98f
learnatalx <gohatsibahw@gmail.com>
2022-10-23 11:09:07 -0700

    add file

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include "main.h"
 #include <stdlib.h>
+/**
+ *_abs - absolute value function
+ *Return: integer always returned
+ */
 int _abs(int c)
 {
 int d;
-d=abs(c);
-return (d);  
+d = abs(c);
+return (d);
 }
