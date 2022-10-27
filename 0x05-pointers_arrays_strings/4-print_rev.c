aff23eafc721709500b2c8833f96acf3799b7e52
learnatalx <gohatsibahw@gmail.com>
2022-10-27 12:17:45 -0700

    add file

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_rev - reverse print
+ * @s: input s
+ * Return: no
+ */
+void print_rev(char *s)
+{
+int count = 0;
+while (count >= 0)
+{
+if (s[count] == '\0')
+break;
+count++;
+}
+for (count--; count >= 0; count--)
+_putchar(s[count]);
+_putchar('\n');
+}
