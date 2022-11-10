5d12eaf69921123797a0e247a0b7d736d7b9c66c
learnatalx <gohatsibahw@gmail.com>
2022-11-09 18:32:54 -0800

    add file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: size of cmd arguments
+ * @argv: array of cl arguments
+ * Return: zero
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
